30eada2f0199f8f19c8d0bb690afe83d7f057e22
ShogoN-OECU <67675781+ShogoN-OECU@users.noreply.github.com>
2021-04-12 03:54:21 +0900

    21/04/12 ShogoN ver.01
    テキストの表示と表示速度の調整を行った

---
+++ Assets/TextWriter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextWriter : MonoBehaviour
+{
+
+    public UIText uitext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Cotest");
+
+    }
+
+    // 文章を表示させるコルーチン(説明雑)
+    IEnumerator Cotest()
+    {
+        uitext.DrawText("あー、あー、テステス");
+        while (uitext.playing) yield return 0;
+        while (!uitext.IsClicked()) yield return 0;
+
+        uitext.DrawTest("テスター", "会話文のテストです。\nついでに改行のチェックもする");
+        while (uitext.playing) yield return 0;
+        while (!uitext.IsClicked()) yield return 0;
+
+        uitext.DrawTest("テスター", "長文を記入した際に改行されるかのチェックだけども文章思いつかない。");
+        while (uitext.playing) yield return 0;
+        while (!uitext.IsClicked()) yield return 0;
+    }
+}
--- Assets/UIText.cs
+++ Assets/UIText.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIText : MonoBehaviour
 {
+    // nameText:喋っている人の名前
+    // talkText:喋っている内容やナレーション
+
+    public Text nameText;
+    public Text talkText;
+
+    public bool playing = false;
+    public float textSpeed = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    // 関数：クリックで次のページを表示させる
+    public bool IsClicked()
     {
-        
+        if (Input.GetMouseButtonDown(0)) return true;
+        return false;
+    }
+
+    // 関数：ナレーション用のテキスト生成
+    public void DrawText ( string text)
+    {
+        nameText.text = "";
+        StartCoroutine("CoDrawText", text);
+    }
+    // 関数：通常会話用のテキスト生成
+    public void DrawTest ( string name, string text)
+    {
+        nameText.text = name + "\n「";
+        StartCoroutine("CoDrawText", text + "」");
+    }
+
+    // テキストがヌルヌル出てくるためのコルーチン
+    IEnumerator CoDrawText ( string text )
+    {
+        playing = true;
+        float time = 0;
+        while ( true )
+        {
+            yield return 0;
+            time += Time.deltaTime;
+
+            // クリックされると一気に表示
+            if ( IsClicked() ) break;
+
+            int len = Mathf.FloorToInt ( time / textSpeed);
+            if (len > text.Length) break;
+            talkText.text = text.Substring(0, len);
+        }
+        talkText.text = text;
+        yield return 0;
+        playing = false;
     }
 }
