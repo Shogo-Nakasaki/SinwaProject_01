d2c740253e8ab231b664a6f2020f777c3893823d
Shogo-Nalasalo <74720278+Shogo-Nakasaki@users.noreply.github.com>
2021-04-13 04:05:21 +0900

    21/04/13 ver.002 ShogoN
    画像の変更ができるかの確認。
    →要画像のサイズ調整
    1体だけと2体並べるときとの対応が必要･･････

---
+++ Assets/Scripts/UIImage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIImage : MonoBehaviour
+{
+    public Image image;
+    // 登場キャラ達
+    public Sprite daiti;
+    public Sprite haru;
+    public Sprite hikari;
+    public Sprite meari;
+    public Sprite mizuki;
+    public Sprite sora;
+
+    private Sprite sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 動作チェック：Zキーを入力するとダイチからハルに画像が変わるか
+        if( Input.GetKeyDown(KeyCode.Z) )
+        {
+            sprite = hikari;
+            // image = this.GetComponent<Image>();
+            image.sprite = sprite;
+        }
+    }
+}
